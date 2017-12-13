c83285a9b167d88acd0f48ed12f7309e959b5241
Brian Gracie <7625540+bgracie@users.noreply.github.com>
2017-12-13 14:23:11 -0500

    Initial commit of empty elixir project

---
+++ lib/domain_planner.ex
@@ -0,0 +1,18 @@
+defmodule DomainPlanner do
+  @moduledoc """
+  Documentation for DomainPlanner.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DomainPlanner.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/domain_planner_test.exs
@@ -0,0 +1,8 @@
+defmodule DomainPlannerTest do
+  use ExUnit.Case
+  doctest DomainPlanner
+
+  test "greets the world" do
+    assert DomainPlanner.hello() == :world
+  end
+end
